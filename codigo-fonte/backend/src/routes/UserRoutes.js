5e6fc17a556e7500047d3c1a668cfff6add2ee63
Lucas Reis Alves <115434561+lucasreisalves@users.noreply.github.com>
2024-10-20 16:46:15 -0300

    Update UserRoutes.js

--- codigo-fonte/backend/src/routes/UserRoutes.js
+++ codigo-fonte/backend/src/routes/UserRoutes.js
@@ -6,7 +6,7 @@ const UserController = require("../controllers/UserController");
 const verifyToken = require("../helpers/verify-token")
 
 router.post("/register", UserController.register);
-router.post("/login", UserController.login);
+
 router.get("/checkuser", UserController.checkUser);
 router.get("/:id", UserController.getUserById);
 router.patch("/edit/:id", verifyToken, UserController.editUser);
