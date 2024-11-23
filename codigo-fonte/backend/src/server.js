26ece23f917bfe1acbe6035f3744a9be513ac86d
elvesdias <elves_92@hotmail.com>
2024-11-23 15:57:04 -0300

    Update server

--- codigo-fonte/backend/src/server.js
+++ codigo-fonte/backend/src/server.js
@@ -42,6 +42,6 @@ app.use('/', ProgramRoutes);
 app.use('/', WorkoutRoutes);
 
 const PORT = 3333;
-app.listen(PORT, function () {
+app.listen(PORT, '0.0.0.0', function () {
   console.log(`Server is running on Port ${PORT}`);
 });
