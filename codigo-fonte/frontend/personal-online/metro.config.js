c5eeaecd0d3084d4d63d0c632a973f32dcc603c0
elvesdias <elves_92@hotmail.com>
2024-11-23 00:03:47 -0300

    Update

--- codigo-fonte/frontend/personal-online/metro.config.js
+++ codigo-fonte/frontend/personal-online/metro.config.js
@@ -14,6 +14,21 @@ module.exports = (() => {
         assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
         sourceExts: [...resolver.sourceExts, "svg"],
     };
+    const { getDefaultConfig } = require("expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...defaultConfig,
+  resolver: {
+    ...defaultConfig.resolver,
+    extraNodeModules: {
+      ...defaultConfig.resolver.extraNodeModules,
+      'normalize-css-color': require.resolve('normalize-css-color')
+    }
+  }
+};
+
 
     return config;
 })();
