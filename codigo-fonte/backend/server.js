41d4348a95c76368a02c0f35aad5e251e994cccf
elvesdias <112361834+elvesdias@users.noreply.github.com>
2024-11-24 18:52:10 -0300

    Update server.js

--- codigo-fonte/backend/server.js
+++ codigo-fonte/backend/server.js
@@ -8,7 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 
+// const PORT = 3333;
+// app.listen(PORT, function () {
+//   console.log(`Server is running on Port ${PORT}`);
+// });
+
+
 const PORT = 3333;
-app.listen(PORT, function () {
+app.listen(PORT, '0.0.0.0', function () {
   console.log(`Server is running on Port ${PORT}`);
 });
