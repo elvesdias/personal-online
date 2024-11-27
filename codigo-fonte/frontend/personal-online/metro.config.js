9a9667cf8dbaac4bdbc8ed3470e9a0ba852f6d63
elvesdias <112361834+elvesdias@users.noreply.github.com>
2024-11-27 19:47:18 -0300

    Update metro.config.js

--- codigo-fonte/frontend/personal-online/metro.config.js
+++ codigo-fonte/frontend/personal-online/metro.config.js
@@ -1,33 +1,28 @@
-module.exports = (() => {
-    const { getDefaultConfig } = require("expo/metro-config");
-    const config = getDefaultConfig(__dirname);
 
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+    const config = getDefaultConfig(__dirname);
     const { transformer, resolver } = config;
-    
+
+    // Configuração para o transformer do React Native SVG
     config.transformer = {
         ...transformer,
         babelTransformerPath: require.resolve("react-native-svg-transformer"),
     };
+
+    // Modificando a resolução de extensões para incluir o SVG
     config.resolver = {
-        ...resolver, 
+        ...resolver,
         assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
         sourceExts: [...resolver.sourceExts, "svg"],
     };
-    
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-module.exports = {
-  ...defaultConfig,
-  resolver: {
-    ...defaultConfig.resolver,
-    extraNodeModules: {
-      ...defaultConfig.resolver.extraNodeModules,
-      'normalize-css-color': require.resolve('normalize-css-color')
-    }
-  }
-};
-
+    // Adicionando a resolução para o 'normalize-css-color'
+    config.resolver.extraNodeModules = {
+        ...resolver.extraNodeModules,
+        'normalize-css-color': require.resolve('normalize-css-color'),
+    };
 
     return config;
 })();
